2314e60da2af54396a55bff70860ba2cfb835d3a
kitamiq <i4v.mail@ya.ru>
2023-04-25 17:11:20 +0300

    fix mail.php

--- mail.php
+++ mail.php
@@ -44,18 +44,18 @@ try {
   $mail->addAddress('info@ai4g.ru');
 
   // Прикрипление файлов к письму
-  if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-      $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-      $filename = $file['name'][$ct];
-      if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-          $mail->addAttachment($uploadfile, $filename);
-          $rfile[] = "Файл $filename прикреплён";
-      } else {
-          $rfile[] = "Не удалось прикрепить файл $filename";
-      }
-    }
-  }
+  //if (!empty($file['name'][0])) {
+  //  for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+  //    $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+  //    $filename = $file['name'][$ct];
+  //    if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+  //        $mail->addAttachment($uploadfile, $filename);
+  //        $rfile[] = "Файл $filename прикреплён";
+  //    } else {
+  //        $rfile[] = "Не удалось прикрепить файл $filename";
+  //   }
+  // }
+  // }
 
   // Отправка сообщения
   $mail->isHTML(true);
